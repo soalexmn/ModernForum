42208b73f9d8aaa3c706ffb53295354425a60a7a
alexgol <massomn@mail.ru>
2017-02-16 14:28:09 +0300

    style first variant

---
+++ src/ModernForum/gulpfile.js
@@ -0,0 +1,44 @@
+﻿/// <binding BeforeBuild='compile-css, compile-main-less, publish-lib' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename')
+
+
+//gulp.task('less-watch', function () {
+//    watch()
+//});
+
+
+
+//gulp.task('project-start', function () {
+//    // place code for your default task here
+//});
+
+gulp.task('compile-main-less', function () {
+    gulp.src('ClientApp/less/main.less')
+        .pipe(less().on('error', function (err) { console.log(err); }))
+        .pipe(gulp.dest('wwwroot/css/'))
+        .pipe(cssmin().on('error', function (err) { console.log(err); }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('wwwroot/css/'));
+});
+
+gulp.task('compile-css', function () {
+    gulp.src('ClientApp/css/*.css')
+        .pipe(gulp.dest('wwwroot/css/'))
+        .pipe(cssmin().on('error', function (err) { console.log(err); }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('wwwroot/css/'));
+});
+
+gulp.task('publish-lib', function () {
+    gulp.src('ClientApp/lib/**')
+        .pipe(gulp.dest('wwwroot/lib/'));
+});
