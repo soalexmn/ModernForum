3995a3bc10dbcfc693b91c2b5cdcb369437ecd30
alexgol <massomn@mail.ru>
2017-02-17 17:23:20 +0300

    navmenu try to change

--- src/ModernForum/ClientApp/app/components/navmenu/navmenu.component.js
+++ src/ModernForum/ClientApp/app/components/navmenu/navmenu.component.js
@@ -11,6 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var NavMenuComponent = (function () {
     function NavMenuComponent() {
+        this.showLeftNav = false;
     }
     NavMenuComponent = __decorate([
         core_1.Component({
--- src/ModernForum/ClientApp/app/components/navmenu/navmenu.component.ts
+++ src/ModernForum/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, animate, trigger, transition, style } from '@angular/core';
+
+
 
 @Component({
     selector: 'nav-menu',
     template: require('./navmenu.component.html'),
     styles: [require('./navmenu.component.css')]
+    //animations: [trigger('enterAnimation', [
+    //    transition(':enter', [
+    //        style({ transform: 'translateX(100%)', opacity: 0 }),
+    //        animate('500ms', style({ transform: 'translateX(0)', opacity: 1 }))
+    //    ]),
+    //    transition(':leave', [
+    //        style({ transform: 'translateX(0)', opacity: 1 }),
+    //        animate('500ms', style({ transform: 'translateX(100%)', opacity: 0 }))
+    //    ])
+    //])]
 })
 export class NavMenuComponent {
+    showLeftNav: boolean = false; 
+
+       
 }
